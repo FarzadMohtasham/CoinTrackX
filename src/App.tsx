c0422d9d27846c6631e47b24d8d5c36f5f3952a1
Farzad Mohtasham <farzaddddmohtasham@gmail.com>
2024-04-02 19:17:00 +0330

    Some improvments

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { JSX } from 'react'
+import {JSX} from 'react'
 import './App.css'
 
 function App(): JSX.Element {
-  return (
-    <div className={'app'}>
-      <h2>Hello World</h2>
-    </div>
-  )
+    return (
+        <div className={'app'}></div>
+    )
 }
 
 export default App
