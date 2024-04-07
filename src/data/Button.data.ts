9d35f0d019e9bec054a41fd3940bff3f31a1adde
Farzad Mohtasham <farzaddddmohtasham@gmail.com>
2024-04-07 17:36:49 +0330

    Code Quality Improved

---
+++ src/data/Button.data.ts
@@ -0,0 +1,47 @@
+export const buttonPaddingVariations = {
+    sm: {
+        x: '2.4rem',
+        y: '1.4rem',
+    },
+    lg: {
+        x: '2.4rem',
+        y: '1.8rem',
+    }
+}
+export const buttonFontSizeVariations = {
+    sm: {
+        fontSize: 'var(--font-size-body-sm)'
+    },
+    lg: {
+        fontSize: 'var(--font-size-body-lg)'
+    }
+}
+export const buttonTypeVariations = {
+    primary: {
+        backgroundColor: 'var(--color-primary)',
+        color: 'white',
+    },
+    secondary: {
+        backgroundColor: 'var(--color-secondary)',
+        color: 'black',
+    },
+    black: {
+        backgroundColor: 'var(--color-black)',
+        color: 'white',
+    },
+    white: {
+        backgroundColor: 'var(--color-white)',
+        color: 'black',
+    },
+    danger: {
+        backgroundColor: 'var(--color-danger)',
+        color: 'white',
+    }
+}
+export const buttonBorderRadius = {
+    none: '0',
+    sm: '0.5rem',
+    md: '0.8rem',
+    lg: '1rem',
+    full: '10rem',
+}
--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -7,53 +7,12 @@ import Icon from './Icon.tsx'
 import {ButtonPropsType} from "../ts/type/ButtonProps.type.ts";
 
 // Data
-const buttonPaddingVariations = {
-    sm: {
-        x: '2.4rem',
-        y: '1.4rem',
-    },
-    lg: {
-        x: '2.4rem',
-        y: '1.8rem',
-    }
-}
-const buttonFontSizeVariations = {
-    sm: {
-        fontSize: 'var(--font-size-body-sm)'
-    },
-    lg: {
-        fontSize: 'var(--font-size-body-lg)'
-    }
-}
-const buttonTypeVariations = {
-    primary: {
-        backgroundColor: 'var(--color-primary)',
-        color: 'white',
-    },
-    secondary: {
-        backgroundColor: 'var(--color-secondary)',
-        color: 'black',
-    },
-    black: {
-        backgroundColor: 'var(--color-black)',
-        color: 'white',
-    },
-    white: {
-        backgroundColor: 'var(--color-white)',
-        color: 'black',
-    },
-    danger: {
-        backgroundColor: 'var(--color-danger)',
-        color: 'white',
-    }
-}
-const buttonBorderRadius = {
-    none: '0',
-    sm: '0.5rem',
-    md: '0.8rem',
-    lg: '1rem',
-    full: '10rem',
-}
+import {
+    buttonTypeVariations,
+    buttonPaddingVariations,
+    buttonFontSizeVariations,
+    buttonBorderRadius
+} from './../data/Button.data.ts'
 
 function Button(props: ButtonPropsType) {
     const {
