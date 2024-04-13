17f9ed073f499d3a1570511e83c4c354d35c0e90
Farzad Mohtasham <farzaddddmohtasham@gmail.com>
2024-04-13 17:50:09 +0330

    Developed Platform component of Home Page

--- src/components/home/Main-Platform.home.tsx
+++ src/components/home/Main-Platform.home.tsx
@@ -1,5 +1,29 @@
+import {css, styled} from "styled-components";
+
+import Container from "../ui/Container.tsx";
+import HeadingBox from "../ui/HeadingBox.tsx";
+
+const PlatformWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 10rem;
+
+  img {
+    width: 100%;
+  }
+`
+
 export default function Platform() {
     return (
-        <div></div>
+        <Container backgroundStyle={css`background: linear-gradient(to top, #f8f7fe, #f8f7fe, #fff);`}>
+            <PlatformWrapper>
+                <HeadingBox label={'platform'}
+                            heading={'Get a bird’s eye view of your crypto assets growing!'}
+                            desc={'With a easy-to-use user interface, you can check your tokens and coins profit/loss in less than 3 seconds, That’s true'}/>
+
+                <img src="/images/platform.home.png"
+                     alt="platform image"/>
+            </PlatformWrapper>
+        </Container>
     )
 }
--- src/ts/type/HeadingBox.type.ts
+++ src/ts/type/HeadingBox.type.ts
@@ -2,5 +2,5 @@ export type HeadingBoxType = {
     label: string;
     heading: string;
     desc: string;
-    headingTag: 'h1' | 'h2' | 'h3' | 'h4';
+    headingTag?: 'h1' | 'h2' | 'h3' | 'h4';
 }
