c183e5da37d2aee2fd3f5c59c05e4f94726fb587
Farzad Mohtasham <farzaddddmohtasham@gmail.com>
2024-04-22 15:59:03 +0330

    Bug fix

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,7 +1,6 @@
-import {createClient} from '@supabase/supabase-js'
-import env from "./../utils/env.ts";
-
-const supabaseClient = createClient(env.SUPABASE_PROJECT_URL, env.SUPABASE_API_KEY)
-
-export default supabaseClient
-
+// import {createClient} from '@supabase/supabase-js'
+// import env from "./../utils/env.ts";
+//
+// const supabaseClient = createClient(env.SUPABASE_PROJECT_URL, env.SUPABASE_API_KEY)
+//
+// export default supabaseClient
