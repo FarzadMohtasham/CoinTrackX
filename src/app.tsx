9ddde9d913b6f6674ce60ed3debd0d27de05bfe6
Farzad Mohtasham <farzaddddmohtasham@gmail.com>
2024-05-07 14:47:34 +0330

    Added React-Query provider

--- src/app.tsx
+++ src/app.tsx
@@ -8,14 +8,27 @@ import {ThemeProvider} from "styled-components";
 import {lightTheme} from './utils/themes.ts'
 import router from "./router.tsx";
 import {Toaster} from "react-hot-toast";
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 60 * 1000,
+        },
+    }
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <Toaster
-            position="top-center"
-        />
-        <ThemeProvider theme={lightTheme}>
-            <RouterProvider router={router}/>
-        </ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools initialIsOpen={false}/>
+            <Toaster
+                position="top-center"
+            />
+            <ThemeProvider theme={lightTheme}>
+                <RouterProvider router={router}/>
+            </ThemeProvider>
+        </QueryClientProvider>
     </React.StrictMode>,
 )
