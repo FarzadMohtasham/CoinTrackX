70285d05d56ee1e20db95244912daff856a981b1
Farzad Mohtasham <farzaddddmohtasham@gmail.com>
2024-05-31 21:51:05 +0330

    Better file import alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,7 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import sass from 'sass'
-import {fileURLToPath, URL} from "url";
-
-const srcFolders = ['assets', 'components', 'config', 'data', 'feature', 'hooks', 'jest', 'layout', 'pages', 'query', 'services', 'styles', 'tests', 'ts', 'utils', 'validation']
-const resolveAlias: { find: string; replacement: string; }[] = []
-srcFolders.map(folderName => {
-    resolveAlias.push({
-        find: `@${folderName}`,
-        replacement: fileURLToPath(new URL(`./src/${folderName}/`, import.meta.url))
-    })
-})
-
-// {
-//     find: "@components",
-//         replacement: fileURLToPath(new URL('./src/components/', import.meta.url))
-// },
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -28,7 +14,27 @@ export default defineConfig({
         }
     },
     resolve: {
-        alias: resolveAlias
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+            "@assets": path.resolve(__dirname, "./src/assets"),
+            "@components": path.resolve(__dirname, "./src/components"),
+            "@/components": path.resolve(__dirname, "./src/components"),
+            "@config": path.resolve(__dirname, "./src/config"),
+            "@data": path.resolve(__dirname, "./src/data"),
+            "@feature": path.resolve(__dirname, "./src/feature"),
+            "@hooks": path.resolve(__dirname, "./src/hooks"),
+            "@jest": path.resolve(__dirname, "./src/jest"),
+            "@layout": path.resolve(__dirname, "./src/layout"),
+            "@pages": path.resolve(__dirname, "./src/pages"),
+            "@query": path.resolve(__dirname, "./src/query"),
+            "@services": path.resolve(__dirname, "./src/services"),
+            "@styles": path.resolve(__dirname, "./src/styles"),
+            "@tests": path.resolve(__dirname, "./src/tests"),
+            "@ts": path.resolve(__dirname, "./src/ts"),
+            "@utils": path.resolve(__dirname, "./src/utils"),
+            "@/lib": path.resolve(__dirname, "./src/lib"),
+            "@validation": path.resolve(__dirname, "./src/validation"),
+        }
     },
     server: {
         port: 3000,
