2a3d6b95f95ef9b8994a6e0cb139f882011cd010
Farzad Mohtasham <48632860+FarzadMohtasham@users.noreply.github.com>
2024-07-16 21:33:26 +0330

    feat: Added Profile page components

---
+++ src/components/dashboard/settings/profile/PersonalInfo.tsx
@@ -0,0 +1,8 @@
+export default function PersonalInfo() {
+
+  return (
+    <div>
+      PersonalInfo
+    </div>
+  )
+}
--- src/pages/dashboard/settings/Profile.page.tsx
+++ src/pages/dashboard/settings/Profile.page.tsx
@@ -1,3 +1,28 @@
+import ContactInfo from '@components/dashboard/settings/profile/ContactInfo.tsx';
+import PersonalInfo from '@components/dashboard/settings/profile/PersonalInfo.tsx';
+import CloseAccount from '@components/dashboard/settings/profile/CloseAccount.tsx';
+import { styled } from 'styled-components';
+
+const ProfileContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+
+    @media screen and (min-width: ${props => props.theme.breakpoints.md}) {
+        padding: 32px;
+    }
+    
+    @media screen and (max-width: ${props => props.theme.breakpoints.md}) {
+        padding: 16px;
+    }
+`
+
 export function Component() {
-  return <div>ProfilePage</div>;
+  return (
+    <ProfileContainer>
+      <ContactInfo/>
+      <PersonalInfo/>
+      <CloseAccount/>
+    </ProfileContainer>
+  );
 }
