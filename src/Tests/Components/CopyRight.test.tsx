e74f3fc1184e4ed96939e0351a3d7c06770b3b3c
Farzad Mohtasham <48632860+FarzadMohtasham@users.noreply.github.com>
2024-08-02 00:32:13 +0330

    test: Added CopyRight component test

---
+++ src/Tests/Components/CopyRight.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@/Libs/Utils/test-utils';
+import CopyRight from '@/Components/UI/Stuff/CopyRight';
+
+const date = new Date();
+
+describe('CopyRight component', () => {
+   it('should output copyRight text with current year', () => {
+      render(<CopyRight />);
+      screen.getByText(`Copyright ${date.getFullYear()} © CoinTrackX`, {
+         exact: false,
+      });
+   });
+});
