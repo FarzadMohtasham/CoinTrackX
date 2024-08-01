a8474721a666a7d0fc898a66e5a95062e676bc0a
Farzad Mohtasham <48632860+FarzadMohtasham@users.noreply.github.com>
2024-08-02 02:22:09 +0330

    test: Added SimpleNotification functionality tests

--- src/Tests/Components/SimpleNotification.test.tsx
+++ src/Tests/Components/SimpleNotification.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@/Libs/Utils/test-utils';
 import SimpleNotification from '@/Components/UI/Notifications/Simple-Notification.notif';
 import { NotificationOptions } from '@/Libs/Typings/Components/Notification.type';
+import userEvent from '@testing-library/user-event';
 
 const date = new Date();
 
@@ -24,5 +25,35 @@ describe('Testing content of notifications', () => {
 
       expect(titleContentEl).toBeInTheDocument();
       expect(messageContentEl).toBeInTheDocument();
+   });
+});
+
+describe('SimpleNotification functionality', () => {
+   it('should be deleted from dom when closed', async () => {
+      const notifOptions: NotificationOptions = {
+         id: 1,
+         title: 'notif title',
+         message: 'notif message',
+         createdAt: date.getDate(),
+         type: 'success',
+         closable: true,
+      };
+      const { container } = render(
+         <SimpleNotification options={notifOptions} />,
+      );
+      const notifContainerEl = container.querySelector('div');
+      const closeNotifEl = container.querySelector('.close-icon');
+      const user = userEvent.setup();
+
+      expect(notifContainerEl).toBeInTheDocument();
+      expect(closeNotifEl).toBeInTheDocument();
+
+      if (closeNotifEl) {
+         await user.click(closeNotifEl);
+         expect(notifContainerEl).not.toBeInTheDocument();
+         expect(closeNotifEl).not.toBeInTheDocument();
+      } else {
+         throw new Error('closeNotifEl is not in the dom tree');
+      }
    });
 });
