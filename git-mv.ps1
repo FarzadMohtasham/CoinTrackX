78b01b09bf58c8737ac855c1e0ba38f2ef4261a1
Farzad Mohtasham <48632860+FarzadMohtasham@users.noreply.github.com>
2024-08-05 19:27:44 +0330

    fix: Fixed files paths diff

---
+++ git-mv.ps1
@@ -0,0 +1,59 @@
+# Renaming Breadcrumb.tsx
+git mv src/components/ui/stuff/Breadcrumb.tsx src/components/ui/stuff/temp_Breadcrumb.tsx
+git mv src/components/ui/stuff/temp_Breadcrumb.tsx src/Components/UI/Stuff/Breadcrumb.tsx
+
+# Renaming Tip.tsx
+git mv src/components/ui/stuff/Tip.tsx src/components/ui/stuff/temp_Tip.tsx
+git mv src/components/ui/stuff/temp_Tip.tsx src/Components/UI/Stuff/Tip.tsx
+
+# Renaming useAuth.ts
+git mv src/libs/hooks/useAuth.ts src/libs/hooks/temp_useAuth.ts
+git mv src/libs/hooks/temp_useAuth.ts src/Libs/Hooks/useAuth.ts
+
+# Renaming User.type.ts
+git mv src/libs/typings/auth/User.type.ts src/libs/typings/auth/temp_User.type.ts
+git mv src/libs/typings/auth/temp_User.type.ts src/Libs/Typings/Auth/User.type.ts
+
+# Renaming creditDebitCard.store.ts
+git mv src/services/stores/paymentMethods/creditDebitCard.store.ts src/services/stores/paymentMethods/temp_creditDebitCard.store.ts
+git mv src/services/stores/paymentMethods/temp_creditDebitCard.store.ts src/Services/Stores/PaymentMethods/creditDebitCard.store.ts
+
+# Renaming user.store.ts
+git mv src/services/stores/user.store.ts src/services/stores/temp_user.store.ts
+git mv src/services/stores/temp_user.store.ts src/Services/Stores/user.store.ts
+
+# Renaming Badge.test.tsx
+git mv src/tests/components/Badge.test.tsx src/tests/components/temp_Badge.test.tsx
+git mv src/tests/components/temp_Badge.test.tsx src/Tests/Components/Badge.test.tsx
+
+# Renaming Button.test.tsx
+git mv src/tests/components/Button.test.tsx src/tests/components/temp_Button.test.tsx
+git mv src/tests/components/temp_Button.test.tsx src/Tests/Components/Button.test.tsx
+
+# Renaming Checkbox.test.tsx
+git mv src/tests/components/Checkbox.test.tsx src/tests/components/temp_Checkbox.test.tsx
+git mv src/tests/components/temp_Checkbox.test.tsx src/Tests/Components/Checkbox.test.tsx
+
+# Renaming CopyRight.test.tsx
+git mv src/tests/components/CopyRight.test.tsx src/tests/components/temp_CopyRight.test.tsx
+git mv src/tests/components/temp_CopyRight.test.tsx src/Tests/Components/CopyRight.test.tsx
+
+# Renaming Heading.test.tsx
+git mv src/tests/components/Heading.test.tsx src/tests/components/temp_Heading.test.tsx
+git mv src/tests/components/temp_Heading.test.tsx src/Tests/Components/Heading.test.tsx
+
+# Renaming Icon.test.tsx
+git mv src/tests/components/Icon.test.tsx src/tests/components/temp_Icon.test.tsx
+git mv src/tests/components/temp_Icon.test.tsx src/Tests/Components/Icon.test.tsx
+
+# Renaming SimpleNotification.test.tsx
+git mv src/tests/components/SimpleNotification.test.tsx src/tests/components/temp_SimpleNotification.test.tsx
+git mv src/tests/components/temp_SimpleNotification.test.tsx src/Tests/Components/SimpleNotification.test.tsx
+
+# Renaming loginValidator.test.ts
+git mv src/tests/validators/loginValidator.test.ts src/tests/validators/temp_loginValidator.test.ts
+git mv src/tests/validators/temp_loginValidator.test.ts src/Tests/Validators/loginValidator.test.ts
+
+# Renaming signupValidator.test.ts
+git mv src/tests/validators/signupValidator.test.ts src/tests/validators/temp_signupValidator.test.ts
+git mv src/tests/validators/temp_signupValidator.test.ts src/Tests/Validators/signupValidator.test.ts
--- src/libs/hooks/useAuth.ts
+++
@@ -1,7 +0,0 @@
-import { supabaseClient } from '@configs/supabase/supabaseConfig.ts';
-
-async function signOut() {
-   await supabaseClient.auth.signOut();
-}
-
-export default { signOut };
